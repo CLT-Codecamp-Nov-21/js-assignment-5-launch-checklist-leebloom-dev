8cc5c48533c9741c12603d0f8101bed0dc60581b
Lee Bloom <leebloomdev@gmail.com>
2022-01-13 16:24:47 -0500

    added validationInput code in scriptHelper.js

--- scriptHelper.js
+++ scriptHelper.js
@@ -17,19 +17,38 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
+    // Pilot Name => name="pilotName"
+    // Co-pilot Name => name="copilotName"
+    // Fuel Level (L) => name="fuelLevel"
+    // Cargo Mass (kg) => name="cargoMass"
+
 	let form = document.querySelector("form");
 
 	form.addEventListener("submit", function(event) {
-		let pilotNameInput = document.querySelector("input[name=pilotName]");
-		let copilotNameInput = document.querySelector("input[name=copilotName]");
-		let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
-		let cargoMassInput = document.querySelector("input[name=cargoMass]");
+		const pilotNameInput = document.querySelector("input[name=pilotName]");
+		const copilotNameInput = document.querySelector("input[name=copilotName]");
+		const fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+		const cargoMassInput = document.querySelector("input[name=cargoMass]");
 		console.log(pilotNameInput.value);
 
+        let emptyField = "";
+        let notAlphabet = "";
+        let notNumber = "";
+
 		if(pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === "") {
-			alert("All fields are required!");
-			event.preventDefault();
-		}       
+			emptyField = "All input fields must be filled.";
+		} else if (isNaN(pilotNameInput.value) === false || isNaN(copilotNameInput.value) === false) {
+            notAlphabet = "Pilot Name and Co-Pilot Name must have non-numeric characters.";
+        } else if (isNaN(fuelLevelInput.value) === true || isNaN(cargoMassInput.value) === true) {
+            notNumber = "Fuel Level and Cargo Mass must only have numeric characters.";
+        } else if (emptyField || notAlphabet || notNumber) {
+            let errorMessage = `${emptyField}${notAlphabet}${notNumber}`
+            alert(errorMessage);
+        } else {
+            formSubmission(event, pilotNameInput, copilotNameInput, fuelLevelInput, cargoMassInput);
+        }
+
+        event.preventDefault();
 	});          
 }
 
